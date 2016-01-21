2cf758720bc070ce9f2229e448d9a5bfa89ef3f0
Craig Rodrigues <rodrigc@FreeBSD.org>
2016-01-21 00:22:40 -0800

    Remove deprecated hudson.Plugin

--- src/main/java/jenkins/plugins/clangscanbuild/ClangScanBuildUtils.java
+++ src/main/java/jenkins/plugins/clangscanbuild/ClangScanBuildUtils.java
@@ -5,15 +5,15 @@ import hudson.model.AbstractBuild;
 
 
 public class ClangScanBuildUtils{
-	
+	public static final String SHORTNAME = "clang-scanbuild-plugin";
 	public static final String REPORT_OUTPUT_FOLDERNAME = "clangScanBuildReports";
 	
 	public static String getIconsPath(){
-		return "/plugin/" + PluginImpl.SHORTNAME + "/icons/";
+		return "/plugin/" + SHORTNAME + "/icons/";
 	}
 	
 	public static String getTransparentImagePath(){
-		return "/plugin/" + PluginImpl.SHORTNAME + "/transparent.png";
+		return "/plugin/" + SHORTNAME + "/transparent.png";
 	}
 	
 	public static FilePath locateClangScanBuildReportFolder( AbstractBuild<?,?> build, String folderName ){
